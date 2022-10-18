b57bb1366ad661432cf1f675684e76f0aaa1eb1a
Ahmed <ahmedaymanelkadah@gmail.com>
2022-10-19 00:18:28 +0200

    side-nav done.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log(`Mody`);
+let menuBtn = document.querySelector(".nav-bars");
+let closeBtn = document.querySelector(".closeBtn");
+let sideNav = document.querySelector(".side-nav");
+menuBtn.addEventListener("click", () => {
+  sideNav.style.display = "block";
+});
+closeBtn.addEventListener("click", close);
+document.onclick = (e) => {
+  if (e.target.id !== "sideNav" && e.target.id !== "menuBars") {
+    close();
+  }
+};
+function close() {
+  sideNav.style.display = "none";
+}
+// ---------------------------------------------
+
+let cartBtn = document.querySelector("selectors");

12d897412b82d79b2b7053d0e96a9ffc80533610
Ahmed <ahmedaymanelkadah@gmail.com>
2022-10-19 16:43:50 +0200

    done..

--- js/main.js
+++ js/main.js
@@ -31,21 +31,27 @@ cartImg.addEventListener("click", () => {
 
 let realCount = 0;
 let trickyCount = 0;
+let cartBoxUl = document.querySelector(".cart-box-prods");
 let cartBoxLi = document.querySelector(".cart-box-prods li");
 
 function theChecker() {
   if (realCount == 0) {
     cartNum.classList.add("disabled");
-    cartBoxLi.innerHTML = `<p>Your cart is empty!</p>`;
+    cartBoxUl.innerHTML = `<li><p>Your cart is empty!</p></li>`;
   } else {
     cartNum.classList.remove("disabled");
     cartNum.innerHTML = realCount;
-    cartBoxLi.innerHTML = `<img src="imgs/product-1-thumbnail.jpg" alt="" />
+    cartBoxUl.innerHTML = `<li>
+    <img src="imgs/product-1-thumbnail.jpg" alt="" />
     <div>
     <h4>Fall Limited Edition Sneakers</h4>
     <p>$125.00 x ${realCount} <span>$${realCount * 125}.00</span></p>
     </div>
-    <i class="fas fa-trash-alt"></i>`;
+    <i class="fas fa-trash-alt"></i>
+    </li>
+    <li class="checkout">
+    <button>Checkout</button>
+    </li>`;
     let deleteBtn = document.querySelector(".cart-box-prods li i");
     deleteBtn.addEventListener("click", () => {
       realCount = 0;
@@ -78,3 +84,58 @@ addBtn.addEventListener("click", () => {
 });
 
 document.onload = theChecker();
+
+// ---------------------------------------------
+let leftArrow = document.querySelector(".left-arrow");
+let rightArrow = document.querySelector(".right-arrow");
+
+const imgs = document.querySelectorAll(".img-select a");
+const imgBtns = [...imgs];
+let imgId = 1;
+imgBtns[0].parentElement.classList.add("active");
+
+imgBtns.forEach((imgItem) => {
+  imgItem.addEventListener("click", (event) => {
+    event.preventDefault();
+    imgId = imgItem.dataset.id;
+    slideImage();
+  });
+});
+
+function slideImage() {
+  const displayWidth = document.querySelector(
+    ".img-showcase img:first-child"
+  ).clientWidth;
+
+  document.querySelector(".img-showcase").style.transform = `translateX(${
+    -(imgId - 1) * displayWidth
+  }px)`;
+  imgBtns.forEach((ele) => {
+    if (ele.getAttribute("data-id") == imgId) {
+      ele.parentElement.classList.add("active");
+    } else {
+      ele.parentElement.classList.remove("active");
+    }
+  });
+}
+
+// window.addEventListener("resize", slideImage);
+
+rightArrow.addEventListener("click", () => {
+  if (imgId == 4) {
+    imgId = 1;
+    slideImage();
+  } else {
+    imgId++;
+    slideImage();
+  }
+});
+leftArrow.addEventListener("click", () => {
+  if (imgId == 1) {
+    imgId = 4;
+    slideImage();
+  } else {
+    imgId--;
+    slideImage();
+  }
+});
